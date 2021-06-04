3c0a884048f8300cfa6f5c054257eb1bdb5bab41
mihett05 <mihett05@yandex.ru>
2021-06-04 13:28:02 +0000

    Added pipenv

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
 #/!bin/bash
 
-docker-compose build
-docker-compose up
+cd src
+
+python3 -m pipenv run python manage.py migrate
+python3 -m pipenv run python manage.py initadmin
+
+python3 -m pipenv run python manage.py runserver 0.0.0.0:8001
